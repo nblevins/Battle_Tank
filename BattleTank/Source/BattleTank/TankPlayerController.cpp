cb91dff6a15744961bd27200fbd53175da82f455
nblevins <nblevins@unknown003ee1c3911c.attlocal.net>
2016-07-04 17:21:46 -0700

    Log for possessed tank

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -5,6 +5,23 @@
 
 
 
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    UE_LOG(LogTemp, Warning, TEXT("Beginning play..."));
+    
+    ATank* ControlledTank = GetControlledTank();
+    
+    if (!ControlledTank) {
+        UE_LOG(LogTemp, Warning, TEXT("No Tank Possesed"));
+    } else {
+        UE_LOG(LogTemp, Warning, TEXT("There is a Tank Possesed"));
+        FString Name = ControlledTank->GetName();
+        UE_LOG(LogTemp, Warning, TEXT("The Posessed Tank is named : %s"), *Name);
+    }
+    
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
