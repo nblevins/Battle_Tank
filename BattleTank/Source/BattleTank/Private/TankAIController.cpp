7e7011373b0fb21864c1dc04f75094b8055bd1c2
nblevins <nblevins@unknown003ee1c3911c.attlocal.net>
2016-07-05 17:10:21 -0700

    Add AI controller for non-player tanks

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+
+
+void ATankAIController::BeginPlay()
+{
+    Super::BeginPlay();
+    
+    UE_LOG(LogTemp, Warning, TEXT("Beginning play..."));
+    
+    ATank* ControlledTank = GetControlledTank();
+    
+    if (!ControlledTank) {
+        UE_LOG(LogTemp, Warning, TEXT("No Tank Possesed"));
+    } else {
+        UE_LOG(LogTemp, Warning, TEXT("There is a Tank Possesed"));
+        FString Name = ControlledTank->GetName();
+        UE_LOG(LogTemp, Warning, TEXT("The Posessed Tank is named : %s"), *Name);
+    }
+    
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+    return Cast<ATank>(GetPawn());
+    
+}
+
